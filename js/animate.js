4b358f5d8e4f3f9a6448193f3ac4382c30976b6a
Erin2021 <appleyummy78@gmail.com>
2024-12-27 18:10:59 +0900

    prettier 설정, 변수명 수정, GSAP효과 1회성으로 수정, body배경스타일 고정

--- js/animate.js
+++ js/animate.js
@@ -1,7 +1,7 @@
 //스크롤 애니메이션 효과
-(function(){
-  let funcObj={
-    f_0:function(){
+(function () {
+  let funcObj = {
+    f_0: function () {
       const tl = gsap.timeline();
       tl.from("#section0 .greeting >h1", {
         opacity: 0,
@@ -12,14 +12,14 @@
         y: 30,
       });
     },
-    f_1:function(){
+    f_1: function () {
       const tl = gsap.timeline();
-      tl.from("#section1 .contents> *",{
-      opacity:0,
-      y:100,
+      tl.from("#section1 .contents> *", {
+        opacity: 0,
+        y: 100,
       });
     },
-    f_2:function(){
+    f_2: function () {
       const tl = gsap.timeline();
       tl.from("#section2 .sec-title >h2", {
         opacity: 0,
@@ -29,16 +29,16 @@
         opacity: 0,
         y: -30,
       });
-      tl.from("#section2 .pub-inner>li",{
-        opacity:0,
-        x:500,
-        stagger:{
-          amount:0.3,
-          from:"random"
+      tl.from("#section2 .pub-inner>li", {
+        opacity: 0,
+        x: 500,
+        stagger: {
+          amount: 0.3,
+          from: "random",
         },
       });
     },
-    f_3:function(){
+    f_3: function () {
       const tl = gsap.timeline();
       tl.from("#section3 .sec-title >h2", {
         opacity: 0,
@@ -48,19 +48,18 @@
         opacity: 0,
         y: -30,
       });
-      tl.from("#section3 .design-list .d-thumbnail",{
+      tl.from("#section3 .design-list .d-thumbnail", {
         opacity: 0,
-        scale:1.5,
-        stagger:{
-          amount:0.5,
-          from:"random",
-          
+        scale: 1.5,
+        stagger: {
+          amount: 0.5,
+          from: "random",
         },
         y: 100,
-        invalidateOnRefresh:true,
-      })
+        invalidateOnRefresh: true,
+      });
     },
-    f_4:function(){
+    f_4: function () {
       const tl = gsap.timeline();
       tl.from("#section4 .sec-title >h2", {
         opacity: 0,
@@ -70,18 +69,17 @@
         opacity: 0,
         y: -30,
       });
-      tl.from("#section4 .plan-con-list .plan-list-inner",{
+      tl.from("#section4 .plan-con-list .plan-list-inner", {
         opacity: 0,
-        scale:1.5,
-        stagger:{
-          amount:0.5,
-          from:"random",
-          
+        scale: 1.5,
+        stagger: {
+          amount: 0.5,
+          from: "random",
         },
-        invalidateOnRefresh:true,
+        invalidateOnRefresh: true,
       });
     },
-    f_5:function(){
+    f_5: function () {
       const tl = gsap.timeline();
       tl.from("#section5 .greeting >h2", {
         opacity: 0,
@@ -93,28 +91,30 @@
       });
     },
   };
-  
-  let section = document.querySelectorAll('section');
-  
-  funcObj['f_0']();  //스크롤과 상관없이 첫번째는 먼저 실행이 되게 함
-  
-  window.addEventListener('scroll', function(){
-  
+
+  let section = document.querySelectorAll("section");
+
+  funcObj["f_0"](); //스크롤과 상관없이 첫번째는 먼저 실행이 되게 함
+  let animationExecuted = Array(section.length).fill(false);
+  animationExecuted[0] = true;
+
+  window.addEventListener("scroll", function () {
+    // 각 섹션이 애니메이션을 실행했는지 추적하는 상태
+
     let scroll = document.scrollingElement.scrollTop;
-  
-    for(let i =0; i<section.length;i++){
+
+    for (let i = 0; i < section.length; i++) {
       //스크롤이 되었을때 섹션이 뷰포트에 들어왔으면 그때 섹션함수를 실행함
       //console.log('높이',section[i].offsetTop)
       //console.log('스크롤높이',scroll)
-
-      if(scroll==section[i].offsetTop){
-        funcObj['f_'+i]();
+      if (scroll == section[i].offsetTop) {
+        // 섹션이 뷰포트에 들어왔고 애니메이션이 아직 실행되지 않았으면 실행
+        if (!animationExecuted[i] && scroll >= section[i].offsetTop) {
+          funcObj["f_" + i](); // 애니메이션 실행
+          animationExecuted[i] = true; // 실행 상태 기록
+        }
         //console.log(i)
       }
-    };
-  
+    }
   });
-  
-  
-  
-})()
+})();
--- js/script.js
+++ js/script.js
@@ -1,198 +1,205 @@
 //파트별 이벤트
-(function(){
+(function () {
   //🎈publishing-마우스호버시 이미지 나오게하기
-const preview = document.querySelector('.preview');
-const pbList = document.querySelectorAll('.publishing-con li')
+  const preview = document.querySelector(".preview");
+  const pbList = document.querySelectorAll(".publishing-con li");
 
-//리스트에 올리면 preview 등장
-pbList.forEach(list =>{
-  if(window.innerWidth>=1200){
-    list.addEventListener('mousemove',(e)=>{
-      preview.style.display = "block";
-      //그타겟의 데이터 셋을 받아옴
-      preview.style.backgroundImage=`url(img/${list.dataset.preview})`;
-
-    });
-    list.addEventListener('mouseleave',()=>{
-      preview.style.display = "none";
-    });
-  }
-})
-
-let x = 0;
-let y = 0;
-let targetX = 0;
-let targetY = 0;
-const speed = 0.1; // 따라오는 속도
-
-
-const PBox=document.querySelector('.publishing-con');
-PBox.addEventListener("mousemove", (e) => {
-  x = e.clientX;
-  y = e.clientY;
-});
-
-const loop = () => {
-  // 애니메이션의 가속, 감속
-  targetX += (x - targetX) * speed ;
-  targetY += (y - targetY) * speed;
-
-
-
-  //하단리스트에 호버시 이미지 위치 조정
-  if(y>600){
-    preview.style.translate="-10% -120%"
-  }else{
-    preview.style.translate="-10% -10%"
-  }
-  preview.style.top = targetY+ "px";
-  preview.style.left = targetX + "px";
-  // console.log(preview.style.top, preview.style.left = targetX)
-  
-  // 재귀적으로 호출
-  window.requestAnimationFrame(loop);
-};
-loop();
-
-
-
-//🎈Profile-마우스가 호버하는 것에 따라 
-//사진
-const Pname=document.querySelectorAll('#section1 .personal p');
-const PnamePic =['img/portrait.jpg','img/portrait2.png','img/portrait3.png','img/portrait4.png',]
-Pname.forEach((value,count)=>{
-  value.addEventListener('mouseover',()=>{
-    document.querySelector('#section1 .p-des-1').style.opacity=0.6;
-    document.querySelector('#section1 .p-des-1').innerHTML=`<img src="${PnamePic[count]}" alt="김태연 사진">`
-  })
-  value.addEventListener('mouseleave',()=>{
-    document.querySelector('#section1 .p-des-1').style.opacity=0;
-  })
-
-})
-
-
-//학업
-const Pclass=document.querySelectorAll('#section1 .class');
-const Pclassdes=['피그마를 사용하여 협업을 할 수 있으며,<br>코드를 작성하여 정보설계, UI설계, 화면설계 등을 구현할 수 있습니다.','본 전공입니다. 아시아 문화원형과 산업에 대해 공부했으며<br>문화현상의 기반을 심도있게 조사할 수 있습니다.','복수전공입니다. 자료구조나 컴퓨터 네트워크등 CS를 공부했고,<br>프론트엔드와 UX연구 위주의 수업을 들었습니다.']
-
-Pclass.forEach((value,count)=>{
-  value.addEventListener('mouseover',()=>{
-    document.querySelector('#section1 .p-des-2').style.opacity=1;
-    document.querySelector('#section1 .p-des-2').innerHTML=`<p>${Pclassdes[count]}</p>`
-  })
-  value.addEventListener('mouseleave',()=>{
-    document.querySelector('#section1 .p-des-2').style.opacity=0;
-  })
-})
-
-//취미
-const Phobby=document.querySelectorAll('#section1 .hobby');
-const Phobbydes=['새로운 언어 배우는 것을 좋아합니다. 일반 수준의 영어와 일본어를 구사할 수 있으며 초급 수준의 프랑스어를 할 수 있습니다.','생존운동으로 시작했지만 이제는 운동을 즐기게 되었습니다.<br>헬스장과 필라테스를 다니고 있습니다.','다양한 분야를 접하는 것을 즐깁니다.<br>최근에는 항공 다큐멘터리에 빠져 있습니다.','계획 없이 카드만 들고 시내를 떠돕니다.<br>이는 생각을 정리하는 데 도움을 줍니다.']
-
-Phobby.forEach((value,count)=>{
-  value.addEventListener('mouseover',()=>{
-    document.querySelector('#section1 .p-des-3').style.opacity=1;
-    document.querySelector('#section1 .p-des-3').innerHTML=`<p>${Phobbydes[count]}</p>`
-  })
-  value.addEventListener('mouseleave',()=>{
-    document.querySelector('#section1 .p-des-3').style.opacity=0;
-  })
-})
-
-
-
-
-
-
-//🎈Design-마우스가 돌아다니는 방향대로 스크롤 되기
-let holder = document.querySelector('.design-list'),
-    wrapper = document.querySelector('.design-con-inner'),
-    designContent= document.querySelector('.contents.design-con'),
-    overflowX, mapPositionX
-
-function onResize(e) {
-  overflowX = holder.offsetWidth - (window.innerWidth/1.2);
-  mapPositionX = gsap.utils.mapRange(0, window.innerWidth, overflowX / 2, overflowX / -2);
-}
-
-function onMouseMove(e) {
-  if (overflowX > 0) {
-    let x = e.clientX || (e.changedTouches && e.changedTouches[0].clientX) || 0;
-    gsap.to(holder, {duration: 1, overwrite: true, ease: "power3", x: mapPositionX(x)});
-  }
-}
-
-window.addEventListener("resize", onResize);
-designContent.addEventListener("mousemove", onMouseMove);
-designContent.addEventListener("touchmove", onMouseMove);
-designContent.addEventListener("pointermove", onMouseMove);
-onResize();
-
-//버튼누르면 바뀌기
-const graphicBtn =document.querySelector(".design-btn button:nth-child(1)")
-const uxuiBtn =document.querySelector(".design-btn button:nth-child(2)")
-const DesignBtn = document.querySelectorAll(".design-btn button")
-
-
-DesignBtn.forEach(Btn=>{
-  Btn.addEventListener('click',(e)=>{
-    console.log(e.target);
-    if(!e.target.classList.contains('active')){//토글바꿔
-      DesignBtn.forEach(BtnEvent=>{
-        BtnEvent.classList.remove("active");
-      })
-      e.target.classList.add('active');
+  //리스트에 올리면 preview 등장
+  pbList.forEach((list) => {
+    if (window.innerWidth >= 1200) {
+      list.addEventListener("mousemove", (e) => {
+        preview.style.display = "block";
+        //그타겟의 데이터 셋을 받아옴
+        preview.style.backgroundImage = `url(img/${list.dataset.preview})`;
+      });
+      list.addEventListener("mouseleave", () => {
+        preview.style.display = "none";
+      });
     }
-    if(graphicBtn.classList.contains('active')){
-      document.querySelector('.graphic-part').classList.add('active');
-      document.querySelector('.UXUI-part').classList.remove('active');
-    }else{
-      document.querySelector('.UXUI-part').classList.add('active');
-      document.querySelector('.graphic-part').classList.remove('active');
+  });
+
+  let x = 0;
+  let y = 0;
+  let targetX = 0;
+  let targetY = 0;
+  const speed = 0.1; // 따라오는 속도
+
+  const PBox = document.querySelector(".publishing-con");
+  PBox.addEventListener("mousemove", (e) => {
+    x = e.clientX;
+    y = e.clientY;
+  });
+
+  const loop = () => {
+    // 애니메이션의 가속, 감속
+    targetX += (x - targetX) * speed;
+    targetY += (y - targetY) * speed;
+
+    //하단리스트에 호버시 이미지 위치 조정
+    if (y > 600) {
+      preview.style.translate = "-10% -120%";
+    } else {
+      preview.style.translate = "-10% -10%";
     }
+    preview.style.top = targetY + "px";
+    preview.style.left = targetX + "px";
+    // console.log(preview.style.top, preview.style.left = targetX)
 
-    //✨✨active바뀔때마다 애니메이션효과로 등장
-  const Dthumbnail=document.querySelectorAll('#section3 .design-list .d-thumbnail');
-  gsap.from(Dthumbnail,{
-    opacity: 0,
-      scale:1.5,
-      stagger:{
-        amount:0.5,
-        from:"random",
-      },
-      y: 100,
-      //invalidateOnRefresh:true,
-  });
-  });
+    // 재귀적으로 호출
+    window.requestAnimationFrame(loop);
+  };
+  loop();
 
-  
-
-})
-
-
-
-
-
-
-
-//🔥Jquery 부분
-$(document).ready(function(){
-  //🎈모바일 햄버거 메뉴 눌렀을 때
-  $('header .hamberger-menu').click((e)=>{
-    (e).preventDefault();
-    $('header .hamberger-menu').toggleClass('active');
-    $('.subNav').toggleClass('active');
-  })
-
-  //🎈모바일로 색션 선택시 자동 닫히기
-  $('header .subNav').click(()=>{
-    setTimeout(()=>{
-      $('header .hamberger-menu').removeClass('active');
-      $('.subNav').removeClass('active');
-    },1000);
+  //🎈Profile-마우스가 호버하는 것에 따라
+  //사진
+  const Pname = document.querySelectorAll("#section1 .personal p");
+  const PnamePic = [
+    "img/portrait.jpg",
+    "img/portrait2.png",
+    "img/portrait3.png",
+    "img/portrait4.png",
+  ];
+  Pname.forEach((value, count) => {
+    value.addEventListener("mouseover", () => {
+      document.querySelector("#section1 .p-des-1").style.opacity = 0.6;
+      document.querySelector("#section1 .p-des-1").innerHTML =
+        `<img src="${PnamePic[count]}" alt="김태연 사진">`;
+    });
+    value.addEventListener("mouseleave", () => {
+      document.querySelector("#section1 .p-des-1").style.opacity = 0;
+    });
   });
 
-});
+  //팝업
+  const Pclass = document.querySelectorAll("#section1 .class");
+  const Pclassdes = [
+    "피그마를 사용하여 협업을 할 수 있으며,<br>코드를 작성하여 정보설계, UI설계, 화면설계 등을 구현할 수 있습니다.",
+    "본 전공입니다. 아시아 문화원형과 산업에 대해 공부했으며<br>문화현상의 기반을 심도있게 조사할 수 있습니다.",
+    "복수전공입니다. 자료구조나 컴퓨터 네트워크등 CS를 공부했고,<br>프론트엔드와 UX연구 위주의 수업을 들었습니다.",
+  ];
 
-})()
+  Pclass.forEach((value, count) => {
+    value.addEventListener("mouseover", () => {
+      document.querySelector("#section1 .p-des-2").style.opacity = 1;
+      document.querySelector("#section1 .p-des-2").innerHTML =
+        `<p>${Pclassdes[count]}</p>`;
+    });
+    value.addEventListener("mouseleave", () => {
+      document.querySelector("#section1 .p-des-2").style.opacity = 0;
+    });
+  });
+
+  //취미
+  const Phobby = document.querySelectorAll("#section1 .hobby");
+  const Phobbydes = [
+    "새로운 언어 배우는 것을 좋아합니다. 일반 수준의 영어와 일본어를 구사할 수 있으며 초급 수준의 프랑스어를 할 수 있습니다.",
+    "생존운동으로 시작했지만 이제는 운동을 즐기게 되었습니다.<br>헬스장과 필라테스를 다니고 있습니다.",
+    "다양한 분야를 접하는 것을 즐깁니다.<br>최근에는 항공 다큐멘터리에 빠져 있습니다.",
+    "계획 없이 카드만 들고 시내를 떠돕니다.<br>이는 생각을 정리하는 데 도움을 줍니다.",
+  ];
+
+  Phobby.forEach((value, count) => {
+    value.addEventListener("mouseover", () => {
+      document.querySelector("#section1 .p-des-3").style.opacity = 1;
+      document.querySelector("#section1 .p-des-3").innerHTML =
+        `<p>${Phobbydes[count]}</p>`;
+    });
+    value.addEventListener("mouseleave", () => {
+      document.querySelector("#section1 .p-des-3").style.opacity = 0;
+    });
+  });
+
+  //🎈Design-마우스가 돌아다니는 방향대로 스크롤 되기
+  let holder = document.querySelector(".design-list"),
+    wrapper = document.querySelector(".design-con-inner"),
+    designContent = document.querySelector(".contents.design-con"),
+    overflowX,
+    mapPositionX;
+
+  function onResize(e) {
+    overflowX = holder.offsetWidth - window.innerWidth / 1.2;
+    mapPositionX = gsap.utils.mapRange(
+      0,
+      window.innerWidth,
+      overflowX / 2,
+      overflowX / -2
+    );
+  }
+
+  function onMouseMove(e) {
+    if (overflowX > 0) {
+      let x =
+        e.clientX || (e.changedTouches && e.changedTouches[0].clientX) || 0;
+      gsap.to(holder, {
+        duration: 1,
+        overwrite: true,
+        ease: "power3",
+        x: mapPositionX(x),
+      });
+    }
+  }
+
+  window.addEventListener("resize", onResize);
+  designContent.addEventListener("mousemove", onMouseMove);
+  designContent.addEventListener("touchmove", onMouseMove);
+  designContent.addEventListener("pointermove", onMouseMove);
+  onResize();
+
+  //버튼누르면 바뀌기
+  const graphicBtn = document.querySelector(".design-btn button:nth-child(1)");
+  const uxuiBtn = document.querySelector(".design-btn button:nth-child(2)");
+  const DesignBtn = document.querySelectorAll(".design-btn button");
+
+  DesignBtn.forEach((Btn) => {
+    Btn.addEventListener("click", (e) => {
+      console.log(e.target);
+      if (!e.target.classList.contains("active")) {
+        //토글바꿔
+        DesignBtn.forEach((BtnEvent) => {
+          BtnEvent.classList.remove("active");
+        });
+        e.target.classList.add("active");
+      }
+      if (graphicBtn.classList.contains("active")) {
+        document.querySelector(".graphic-part").classList.add("active");
+        document.querySelector(".UXUI-part").classList.remove("active");
+      } else {
+        document.querySelector(".UXUI-part").classList.add("active");
+        document.querySelector(".graphic-part").classList.remove("active");
+      }
+
+      //✨✨active바뀔때마다 애니메이션효과로 등장
+      const Dthumbnail = document.querySelectorAll(
+        "#section3 .design-list .d-thumbnail"
+      );
+      gsap.from(Dthumbnail, {
+        opacity: 0,
+        scale: 1.5,
+        stagger: {
+          amount: 0.5,
+          from: "random",
+        },
+        y: 100,
+        //invalidateOnRefresh:true,
+      });
+    });
+  });
+
+  //🔥Jquery 부분
+  $(document).ready(function () {
+    //🎈모바일 햄버거 메뉴 눌렀을 때
+    $("header .hamberger-menu").click((e) => {
+      e.preventDefault();
+      $("header .hamberger-menu").toggleClass("active");
+      $(".subNav").toggleClass("active");
+    });
+
+    //🎈모바일로 색션 선택시 자동 닫히기
+    $("header .subNav").click(() => {
+      setTimeout(() => {
+        $("header .hamberger-menu").removeClass("active");
+        $(".subNav").removeClass("active");
+      }, 1000);
+    });
+  });
+})();
